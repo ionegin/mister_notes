c7fd023b96149f05664fa6b39dfc0d3af97dfcf0
ionegin <onegilya@gmail.com>
2026-02-23 00:21:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROQ_KEY = os.getenv("GROQ_API_KEY")
 LLM_KEY = os.getenv("LLM_API_KEY")
+ELEVENLABS_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 # Путь к папке для временных файлов
 TEMP_DIR = "data/temp_audio"
